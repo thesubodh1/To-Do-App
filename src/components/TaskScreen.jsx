84fbe1b77397fcdab71fb4c950d5a7ce8b995e27
thesubodh1 <thesubodh1@gmail.com>
2025-11-09 16:06:17 +0545

    Tasks added with some design

--- src/components/TaskScreen.jsx
+++ src/components/TaskScreen.jsx
@@ -2,8 +2,8 @@ export default function TaskScreen({allTasks}){
     return(
         <ul className="task-lists">
             {allTasks.map((task) => (
-                <li key={task.id}>
-                    <button>{task.title}</button>
+                <li key={task.id} className="tasks">
+                    <button className="tasks-btn">{task.title}</button>
                 </li>
             ))}
         </ul>
